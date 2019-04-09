9d6498345dd56cd5e5710a1dd485d576e3449647
Alex VanBeekum <ajvanbeekum@gmail.com>
2019-04-09 18:59:05 -0500

    Initial Commit

---
+++ inclass-scene2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  inclass-scene2
+//
+//  Created by Alexander VanBeekum on 4/9/19.
+//  Copyright © 2019 Alexander VanBeekum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ inclass-scene2Tests/inclass_scene2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  inclass_scene2Tests.swift
+//  inclass-scene2Tests
+//
+//  Created by Alexander VanBeekum on 4/9/19.
+//  Copyright © 2019 Alexander VanBeekum. All rights reserved.
+//
+
+import XCTest
+@testable import inclass_scene2
+
+class inclass_scene2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
